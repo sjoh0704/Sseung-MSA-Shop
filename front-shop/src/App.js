38ab3cbe15a3caaefec3296ac415070c4d75eb02
seungjuoh <sjoh0704@daum.net>
2021-06-26 01:23:37 +0900

    add register page

--- front-shop/src/App.js
+++ front-shop/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import Logout from './components/Logout';
+import Register from './components/Register'
 function App() {
   return (
    <div>
     {/* <Login></Login> */}
-    <Logout></Logout>
+    {/* <Logout></Logout> */}
+    <Register></Register>
    </div>
   );
 }
