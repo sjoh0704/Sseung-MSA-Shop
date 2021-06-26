615a63a8a2279e156b753bf5d904f7bdef64a3e6
seungjuoh <sjoh0704@daum.net>
2021-06-26 10:47:10 +0900

    add home component

--- front-shop/src/App.js
+++ front-shop/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import Register from './components/Register'
+import Home from './components/Home';
 function App() {
   return (
    <div>
     {/* <Login></Login> */}
     {/* <Logout></Logout> */}
-    <Register></Register>
+    {/* <Register></Register> */}
+    <Home/>
    </div>
   );
 }
