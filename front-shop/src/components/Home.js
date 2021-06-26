e4285ad417e6c709ab77e809c5a481bf48826c85
seungjuoh <sjoh0704@daum.net>
2021-06-26 16:34:45 +0900

    add card

--- front-shop/src/components/Home.js
+++ front-shop/src/components/Home.js
@@ -1,10 +1,11 @@
 import React, {Component, useEffect, useState} from 'react';
 import axios from "axios";
 // import ComplexNavigationBar from "../Common/ComplexNavigationBar/ComplexNavigationBar";
-import {Container, Grid} from "@material-ui/core";
 // import Post from "../LoggeHome/Post";
 // import ComplexNavigationNonLogged from "./ComplexNavigationNonLogged";
 import Carousel from "react-bootstrap/Carousel";
+import {Button, Container, Row,Col, Card} from 'react-bootstrap'
+
 // import NonLoggedPost from "./NonLoggedPost";
 
 export default function Home(props){
@@ -36,22 +37,48 @@ export default function Home(props){
 
         return (
             <div>
+
+                <div>
+                    seungju shop
+                </div>
                 <div style={{marginTop:70}}>
                 <ControlledCarousel/>
-                </div>
-                <Container maxWidth={"xl"} style={{marginLeft:'5%'}}>
-                    <Grid container spacing={1} style={{display:"flex",marginTop:'50px'}}>
-                        {
-                            products.map(product=>{
-                                return (
-                                    <Grid container item xs={4} spacing={4} style={{margin:'00px'}} className="gridItem">
-                                        {/* <NonLoggedPost product={product} dispatch={props.dispatch} setproduct={props.setproduct}/> */}
-                                    </Grid>
-                                )
-                            })
-                        }
-                    </Grid>
+                <Container>
+                <Row>
+                    <Col xs>
+                    <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col xs={{ order: 12 }}>상품 2</Col>
+                    <Col xs={{ order: 1 }}>상품 3</Col>
+                </Row>
+                <Row>
+                    <Col xs>First, but unordered</Col>
+                    <Col xs={{ order: 12 }}>Second, but last</Col>
+                    <Col xs={{ order: 1 }}>Third, but second</Col>
+                </Row>
+                <Row>
+                    <Col xs>First, but unordered</Col>
+                    <Col xs={{ order: 12 }}>Second, but last</Col>
+                    <Col xs={{ order: 1 }}>Third, but second</Col>
+                </Row>
+                <Row>
+                    <Col xs>First, but unordered</Col>
+                    <Col xs={{ order: 12 }}>Second, but last</Col>
+                    <Col xs={{ order: 1 }}>Third, but second</Col>
+                </Row>
                 </Container>
+
+                </div>
             </div>
         );
 }
@@ -102,7 +129,7 @@ function ControlledCarousel() {
                 <Carousel.Caption>
                     <h3>Third slide label</h3>
                     <p>
-                    세번째 
+                    세번째
                     </p>
                 </Carousel.Caption>
             </Carousel.Item>
--- front-shop/src/index.js
+++ front-shop/src/index.js
@@ -7,7 +7,7 @@ import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 import rootReducer from './modules'
 import {composeWithDevTools} from 'redux-devtools-extension'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
