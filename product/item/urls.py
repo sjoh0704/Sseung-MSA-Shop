481b4aad3e99ed36e57606dac7556ba9b76e3e2d
seungjuoh <sjoh0704@daum.net>
2021-07-01 02:21:37 +0000

    add requirements

--- product/item/urls.py
+++ product/item/urls.py
@@ -2,17 +2,12 @@ from django.urls import path
 from .views import ProductNonParam, ProductStatusView, GetCategory,GetProductByCategory, DeleteProductCascadingUser
 urlpatterns = [
 
-    path('product/', ProductNonParam.as_view()),
+    path('product', ProductNonParam.as_view()),
     path('product/<int:pk>', ProductStatusView.as_view()),
-    path('category/', GetCategory.as_view()),
+    path('category', GetCategory.as_view()),
     path('category/<int:pk>', GetProductByCategory.as_view()),
     
-    path('user/<int:pk>/product/', DeleteProductCascadingUser.as_view()),
-    
-
-    # path('login', UserLoginView.as_view(), name='api_v1_user_login'),
-    # path('logout', UserLogoutView.as_view(), name='api_v1_user_logout'),
-    
+    path('product/user/<int:pk>', DeleteProductCascadingUser.as_view()),
     
     
 ]
