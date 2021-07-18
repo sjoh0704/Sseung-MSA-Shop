47c80d47c2e097831b8f8acfa151711a9882da90
seungjuoh <sjoh0704@daum.net>
2021-07-18 22:55:52 +0900

    add sales detail

--- front-shop/src/components/MySalesDetail.js
+++ front-shop/src/components/MySalesDetail.js
@@ -8,35 +8,65 @@ import placeholder from '../images/placeholder2.jpg'
 
 
 function MySalesDetail({history, match}){
-    var products = []
     const [orders, setOrders] = useState([]) 
     const {isLoggedIn, userData} = useSelector(state =>({
         isLoggedIn: state.user.isLoggedIn,
         userData: state.user.payload
     }))
+    const[images,setImages]= useState([])
+    const[product,setProduct]= useState({})
+    const fetchProduct= async ()=>{
+        await axios.get('/apis/v1/product/' + match.params.number).then(res=> {
+            console.log(res.data)
+            setProduct({
+            ...res.data.payload.payload,
+            product_id: match.params.number
+            });
+            let image_list = res.data.payload.payload.image.map(img => (<div >
+                <br/>
+                <img style = {{  
+                    width: "40vw",
+                    }} src={img}></img>
+                <br/>
+                <br/>
+                
+            </div>))
+            setImages(image_list);
+            
+        })
+        .catch(e => {
+            // 정보가 없을 때 처리
+            console.log('없는 상품 정보')
+        })
+    }
+    
+    
 
 
 
     const fetchOrders= async()=>{
             await axios.get('/apis/v1/order/sale/' + match.params.number).then(res=> {
-                console.log(res.data.payload.payload)
+      
                 let tmp_orders = res.data.payload.payload.filter(order=> order.sales_stage!='SO')
+                
+                console.log(tmp_orders)
                 let orderlist = tmp_orders.map((order, index) => {
                     return (
                             
                              <ListGroup.Item key={index}>
                             <Row style={{margin:20}}>
-                            <Col md={4}>
-                            <img style={{ height: '12vw', width:'18vw'}} src={order.base64_image_url?order.base64_image_url:placeholder}></img>
-                            </Col>
-                            <Col md={8}>
+                            <Col>
                            <div>
-                            
-                            <p>
-                            상품명: {order.name}
+                           <p>
+                            구매자: {order.user_name}
                             </p>
                             <p>
-                            수량: {order.demand_quantity}
+                            구매자 이메일: {order.user_email}
+                            </p>
+                            
+                           
+                            <p>
+                            구매량: {order.demand_quantity}
                             </p>
                             <p>
                             지불 금액: {order.price}
@@ -68,6 +98,7 @@ function MySalesDetail({history, match}){
 
 
     useEffect(()=>{
+        fetchProduct()
         fetchOrders()
     },[userData.user_id])
     // console.log(orders.length)
@@ -88,9 +119,28 @@ function MySalesDetail({history, match}){
     return (<div>
         <Title title="주문 목록" set_middle={false}></Title>
         <Container>
-          
+        <Row className="justify-content-md-center">
+                <Col>
+                <ListGroup>
+            <ListGroup.Item>상품 카테고리: {product.category}</ListGroup.Item>
+            <ListGroup.Item>상품명: {product.name}</ListGroup.Item>
+            <ListGroup.Item>상품 가격: {product.price}</ListGroup.Item>
+            <ListGroup.Item>상품 수량: {product.quantity}</ListGroup.Item>
+            <ListGroup.Item>상품 설명: {product.description}</ListGroup.Item>
+            <ListGroup.Item>상품 등록일:{product.created_at}</ListGroup.Item>
+           
+            <ListGroup.Item >{images}</ListGroup.Item>
+            </ListGroup>
+        <br/>
+        </Col>
+        </Row>
         <ListGroup>
-            {orders}
+            {orders.length==0?
+            <div>
+            <br/>
+            <p>구매한 사람이 없습니다.</p>
+            </div>
+            :orders}
         </ListGroup>
         </Container>
         
