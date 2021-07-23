9c6dc9b298cbb0e13967394b60e6b6df32cad134
seungjuoh <sjoh0704@daum.net>
2021-07-23 23:46:50 +0900

    merge

--- account/account/settings.py
+++ account/account/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jqk_k3tt%10%om!2-m9imf^41u*yt(mmavsn2oj+*dke7&^580'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
