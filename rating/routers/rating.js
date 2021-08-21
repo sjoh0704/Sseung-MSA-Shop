7f0fc69fc00ebb5b78b5b44d944e5aef80958124
seungjuoh <sjoh0704@daum.net>
2021-08-21 19:07:04 +0900

    finish rating server

--- rating/routers/rating.js
+++ rating/routers/rating.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Rating = require('../models/rating');
 
+
 // get all ratings
 router.get('/ratings', async(req, res)=> {
     const ratings = await Rating.find({}).exec();
@@ -9,93 +10,84 @@ router.get('/ratings', async(req, res)=> {
         message: 'get ratings success'});
 })
 
+
+// get specific user rating info
+router.get('/ratings/:userId', async(req, res)=> {
+    const {userId} = req.params;
+    const rating = await Rating.findOne({userId}).exec();
+    if(!rating){
+        res.status(400).send({message: 'user rating info 없음'});
+        return;
+    }
+    res.send({payload: rating,
+        message: 'get ratings success'});
+})
+
+
 // create rating
 router.post('/ratings', async(req, res)=> {
     const {userId} = req.body;
     if(!userId){
-        res.status(400).send('userId 없음');
+        res.status(400).send({message: 'userId 없음'});
         return;
     }
     const check = await Rating.find({userId}).exec(); 
     if(check.length){
-        res.status.send('이미 존재하는 사용자입니다.');
+        res.status(400).send({message: '이미 존재하는 사용자입니다.'});
         return;
     }
-
     const rating = new Rating({userId});
     await rating.save();
-
     res.send({message: 'rating 생성 성공'});
 })
 
-// get carts by user(buyer) 
-router.get('/carts/users/:buyerId', async(req, res)=> {
-    const {buyerId} = req.params;
-    const cartsByUser = await Cart.find({buyerId}).exec();
-    console.log(cartsByUser);
 
-    res.send({
-        payload: cartsByUser,
-        message: 'get carts by user success'});
-})
 
-// cart check 
-router.post('/carts/check', async(req, res)=> {
-    console.log
-    const {product_id, buyer_id, seller_id} = req.body;
-    const cartCheck = await Cart.find({$and: [{productId: product_id},{buyerId: buyer_id}, {sellerId: seller_id}]}).exec(); 
-    console.log(cartCheck);
-    if(cartCheck.length === 0){
-        res.send({
-            message: "cart item doesn't exist",
-            payload:{
-                checked: false
-            }
-        })
+// rating up 
+router.get('/ratings/:userId/up', async(req, res)=> {
+    const {userId} = req.params;
+    if(!userId){
+        res.status(400).send({message: 'userId 없음'});
         return;
     }
-    res.send({
-        payload: {
-        ...cartCheck,
-        checked: true
-        },
-        message: 'cart item exist'});
+    const rating = await Rating.findOne({userId});
+    if(!rating){
+        res.status(400).send({message: 'user rating info 없음'});
+        return;
+    }
+    rating.temperature += 0.5;
+    await rating.save();
+    res.send({message: 'user rating up 성공'});
+})
+
+// rating up 
+router.get('/ratings/:userId/down', async(req, res)=> {
+    const {userId} = req.params;
+    if(!userId){
+        res.status(400).send({message: 'userId 없음'});
+        return;
+    }
+    const rating = await Rating.findOne({userId});
+    if(!rating){
+        res.status(400).send({message: 'user rating info 없음'});
+        return;
+    }
+    rating.temperature -= 0.5;
+    await rating.save();
+    res.send({message: 'user rating down 성공'});
 })
 
 
-//cart delete 
-router.delete('/carts/:cartId', async(req, res)=> {
-    const {cartId} = req.params;
-    let cart = null;
-    try{
-        cart = await Cart.findById(cartId).exec();
-
-    // console.log(cart);  
-        await cart.delete();
-        res.send({message: 'delete cart success'});
+//rating delete 
+router.delete('/ratings/:userId', async(req, res)=> {
+    const {userId} = req.params;
+    const rating = await Rating.findOne({userId}).exec();
+    if(!rating){
+        res.status(400).send({message:'rating info 없음'})
+        return;
     }
-    catch{
-        res.status(400).send({
-            message: "cart delete fails"
-        });
-    }
-
-})
-
-
-//cart by product
-router.delete('/product/:productId/carts', async(req, res)=> {
-    const {productId} = req.params;
-    let cart = null;
-    try{
-        cart = await Cart.deleteMany({productId}).exec();
-        res.send({message: 'delete cart success'});
-    }
-    catch{
-        res.status(400).send({
-            message: "cart delete fails"
-        });
-    }
+    await rating.delete();
+    res.send({message: 'rating delete 성공'});
 
 })
 
