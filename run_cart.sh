d06b07e29183a4a8c4a5095115a7eb7d2712e695
seungjuoh <sjoh0704@daum.net>
2021-09-10 21:17:32 +0900

    change run cart

--- run_cart.sh
+++ run_cart.sh
@@ -1,7 +1,8 @@
 echo "-----------------------------------------------------------------------"
 echo "configure envrionment setting"
-export MONGO_CART_HOST=172.17.0.4
-export MONGO_RATING_HOST=172.17.0.4 
+MONGODB=$(docker inspect mongodb-container| jq ".[0].NetworkSettings.IPAddress")
+export MONGO_CART_HOST=${MONGODB:1:-1}
+export MONGO_RATING_HOST=${MONGODB:1:-1}
 echo "-----------------------------------------------------------------------"
 echo "migrate and activate server!"
 node ./cart/app.js
